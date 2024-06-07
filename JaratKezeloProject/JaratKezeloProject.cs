b440ff9e7559919988cd6b18fbd0bb72fd4ba50b
DomokosBenedek <75673497+DomokosBenedek@users.noreply.github.com>
2024-06-07 13:30:34 +0200

    Implemented JaratKezelo class and unit tests

---
+++ JaratKezeloProject/JaratKezeloProject.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JaratKezeloProject
+{
+    public class JaratKezelo
+    {
+        private Dictionary<string, Jarat> jaratok = new Dictionary<string, Jarat>();
+
+        public void UjJarat(string jaratSzam, string repterHonnan, string repterHova, DateTime indulas)
+        {
+            if (jaratok.ContainsKey(jaratSzam))
+                throw new ArgumentException("A járatszám már létezik.");
+
+            jaratok[jaratSzam] = new Jarat
+            {
+                JaratSzam = jaratSzam,
+                RepterHonnan = repterHonnan,
+                RepterHova = repterHova,
+                Indulas = indulas,
+                Keses = 0
+            };
+        }
+
+        public void Keses(string jaratSzam, int keses)
+        {
+            if (!jaratok.ContainsKey(jaratSzam))
+                throw new ArgumentException("Nem létező járat.");
+
+            jaratok[jaratSzam].Keses += keses;
+
+            if (jaratok[jaratSzam].Keses < 0)
+                throw new ArgumentException("A késés nem lehet negatív.");
+        }
+
+        private class Jarat
+        {
+            public string JaratSzam { get; set; }
+            public string RepterHonnan { get; set; }
+            public string RepterHova { get; set; }
+            public DateTime Indulas { get; set; }
+            public int Keses { get; set; }
+        }
+    }
+}
--- TestJaratKezeloProject/UnitTest1.cs
+++ TestJaratKezeloProject/UnitTest1.cs
@@ -1,11 +1,48 @@
+using System;
+using Xunit;
+using JaratKezeloProject;
+
 namespace TestJaratKezeloProject
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void UjJarat_Letrehoz_Es_Tarol()
         {
+            var jaratKezelo = new JaratKezelo();
+            jaratKezelo.UjJarat("ABC123", "BUD", "JFK", DateTime.Now);
 
+            // Ellenõrizd, hogy a járat ténylegesen hozzá lett-e adva (nincs kivétel)
+            jaratKezelo.Keses("ABC123", 0);
+        }
+
+        [Fact]
+        public void Keses_Hozzaadodik()
+        {
+            var jaratKezelo = new JaratKezelo();
+            jaratKezelo.UjJarat("ABC123", "BUD", "JFK", DateTime.Now);
+            jaratKezelo.Keses("ABC123", 10);
+
+            // Ellenõrizd, hogy a késés hozzáadódott
+            jaratKezelo.Keses("ABC123", 0);  // Ha ez nem dob kivételt, a késés sikeresen hozzáadódott
+        }
+
+        [Fact]
+        public void Keses_Nem_Lehet_Negativ()
+        {
+            var jaratKezelo = new JaratKezelo();
+            jaratKezelo.UjJarat("ABC123", "BUD", "JFK", DateTime.Now);
+
+            Assert.Throws<ArgumentException>(() => jaratKezelo.Keses("ABC123", -10));
+        }
+
+        [Fact]
+        public void UjJarat_Duplikalt_JaratSzam()
+        {
+            var jaratKezelo = new JaratKezelo();
+            jaratKezelo.UjJarat("ABC123", "BUD", "JFK", DateTime.Now);
+
+            Assert.Throws<ArgumentException>(() => jaratKezelo.UjJarat("ABC123", "BUD", "JFK", DateTime.Now));
         }
     }
 }
